541f7ffbac522b930c9a4b9313abf9c279aa21e8
Nemanja Miljkovic <proof1337@gmail.com>
2015-11-29 14:44:39 +0100

    Convert windows back slashes to forward slashes.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 var Transform = require('readable-stream/transform');
 var rs = require('replacestream');
 
+var jsExtensionRegex = /\.js/;
+var windowsBackwardSlashRegex = /\\/g;
+
 module.exports = function () {
     return new Transform({
         objectMode: true,
@@ -15,7 +18,10 @@ module.exports = function () {
                 return callback(Error('Only streams and buffers are supported.'), file);
             }
 
-            var name = file.relative.replace(/\.js/, '');
+            var name = file.relative
+                .replace(jsExtensionRegex, '')
+                .replace(windowsBackwardSlashRegex, '/');
+
             var search = /^System\.register\(\[/;
             var replace = "System.register('" + name + "', [";
 
